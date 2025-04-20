b09ae5ffd5d64497a1be2cc771c43435c46fd3f1
Malin Hanak <malinehanak@gmail.com>
2025-04-20 15:22:55 +0200

    Added font and weights

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ import Navigation from "./organisms/Navigation";
 import Main from "./atoms/Main";
 import UserPanel from "./organisms/UserPanel";
 import { Outlet } from "react-router";
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/600.css";
+import "@fontsource/poppins/700.css";
+import "@fontsource/poppins/900.css";
 
 function App() {
   return (
